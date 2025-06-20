72101541499553d6b768884ff9880f729f914cf5
Viktoria <vikina0929@gmail.com>
2025-06-20 09:46:23 -0400

    Correcting flake8 errors

--- service/__init__.py
+++ service/__init__.py
@@ -6,9 +6,9 @@ from flask import Flask
 app = Flask(__name__)
 
 # This must be imported after the Flask app is created
-from service import routes               
+from service import routes             
 # pylint: disable=wrong-import-position,cyclic-import
-from service.common import log_handlers  
+from service.common import log_handlers
 # pylint: disable=wrong-import-position
 
 log_handlers.init_logging(app, "gunicorn.error")
