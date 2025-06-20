376034960223ad617b4681541901b37efc052e89
Viktoria <vikina0929@gmail.com>
2025-06-20 09:05:38 -0400

    Correcting flake8 errors

--- service/routes.py
+++ service/routes.py
@@ -40,7 +40,8 @@ def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
 
-    counters = [dict(name=count[0], counter=count[1]) for count in COUNTER.items()]
+    counters = ([dict(name=count[0], counter=count[1]) 
+    for count in COUNTER.items()])
 
     return jsonify(counters)
 
@@ -54,7 +55,8 @@ def create_counters(name):
     app.logger.info("Request to Create counter: %s...", name)
 
     if name in COUNTER:
-        return abort(status.HTTP_409_CONFLICT, f"Counter {name} already exists")
+        return (abort(status.HTTP_409_CONFLICT, 
+        f"Counter {name} already exists"))
 
     COUNTER[name] = 0
 
@@ -75,7 +77,8 @@ def read_counters(name):
     app.logger.info("Request to Read counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
+        return (abort(status.HTTP_404_NOT_FOUND, 
+        f"Counter {name} does not exist"))
 
     counter = COUNTER[name]
     return jsonify(name=name, counter=counter)
@@ -90,7 +93,8 @@ def update_counters(name):
     app.logger.info("Request to Update counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
+        return (abort(status.HTTP_404_NOT_FOUND, 
+        f"Counter {name} does not exist"))
 
     COUNTER[name] += 1
 
